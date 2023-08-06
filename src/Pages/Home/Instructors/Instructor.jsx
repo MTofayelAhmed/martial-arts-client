25ff71a9e49509432534de0c80dc460b1d91b90e
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-06 18:52:41 +0600

    responsive done

--- src/Pages/Home/Instructors/Instructor.jsx
+++ src/Pages/Home/Instructors/Instructor.jsx
@@ -13,7 +13,7 @@ const [instructors] = useInstructors()
   return (
  <div >
   <SectionTile heading="top Instructors" subHeading='summer camp'></SectionTile>
-     <div className="grid grid-cols-3 mx-20 gap-5 mt-20">
+     <div className="grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 mx-20 gap-5 mt-20">
     {
       instructors.map(instructor=> <InstructorCard key= {instructor._id} instructor= {instructor}></InstructorCard> )
 
